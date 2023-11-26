3c17fe32ae417fb8d7115f9257e946d71bb5ac0a
hakan.kaya.1@gmx.de <hakan.kaya.1@gmx.de>
2023-11-26 20:42:32 +0100

    Unnötige Ordner entfernt und neue erstellt

---
+++ KerlkraftSolution/KerlkraftwerkGame/Graphics/Sprite.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KerlkraftwerkGame.Graphics
+{
+    public class Sprite
+    {
+
+        public Texture2D Texture { get; set; }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public Color TintColor { get; set; } = Color.White;
+
+        public Sprite(Texture2D texture, int x, int y, int width, int height)
+        {
+            this.Texture = texture;
+            this.X = x;
+            this.Y = y;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 position)
+        {
+
+            spriteBatch.Draw(Texture, position, new Rectangle(X, Y, Width, Height), TintColor);
+
+        }
+
+    }
+}
---
+++ KerlkraftSolution/KerlkraftwerkGame/System/InputController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualBasic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+//using TrexRunner.Entities;
+
+namespace KerlkraftwerkGame.System
+{
+    public class InputController
+    {
+
+        private bool isBlocked;
+        private character character;
+
+        private KeyboardState previousKeyboardState;
+
+        public InputController(Trex character)
+        {
+            character = character;
+        }
+
+        public void ProcessControls(GameTime gameTime)
+        {
+
+            KeyboardState keyboardState = Keyboard.GetState();
+
+            if (!isBlocked)
+            {
+                bool isJumpKeyPressed = keyboardState.IsKeyDown(Keys.Up) || keyboardState.IsKeyDown(Keys.Space);
+                bool wasJumpKeyPressed = previousKeyboardState.IsKeyDown(Keys.Up) || previousKeyboardState.IsKeyDown(Keys.Space);
+
+                if (!wasJumpKeyPressed && isJumpKeyPressed)
+                {
+
+                    if (character.State != TrexState.Jumping)
+                        character.BeginJump();
+
+                }
+                else if (character.State == TrexState.Jumping && !isJumpKeyPressed)
+                {
+
+                    character.CancelJump();
+
+                }
+                else if (keyboardState.IsKeyDown(Keys.Down))
+                {
+
+                    if (character.State == TrexState.Jumping || character.State == TrexState.Falling)
+                        character.Drop();
+                    else
+                        character.Duck();
+
+                }
+                else if (character.State == TrexState.Ducking && !keyboardState.IsKeyDown(Keys.Down))
+                {
+
+                    character.GetUp();
+
+                }
+
+            }
+
+            previousKeyboardState = keyboardState;
+
+            isBlocked = false;
+
+        }
+
+        public void BlockInputTemporarily()
+        {
+            isBlocked = true;
+        }
+
+    }
+}
