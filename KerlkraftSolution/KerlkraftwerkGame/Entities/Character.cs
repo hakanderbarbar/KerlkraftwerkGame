4048803b441ebef6943960f448947a12067468e1
hakan.kaya.1@gmx.de <hakan.kaya.1@gmx.de>
2023-12-09 20:21:19 +0100

    Sprungfunktion done

--- KerlkraftSolution/KerlkraftwerkGame/Entities/Character.cs
+++ KerlkraftSolution/KerlkraftwerkGame/Entities/Character.cs
@@ -2,8 +2,12 @@
 {
     private Texture2D texture;
     private Vector2 position;
-    private float jumpSpeed = -10f;  // Geschwindigkeit des Sprungs
+    private float jumpSpeed = 300f;
+    private float gravity = 500f;
     private bool isJumping = false;
+    private float jumpDuration = 0.5f;
+    private float jumpTimer = 0f;
+    private float groundLevel = 430f; // Startposition Character
 
     public Character(Texture2D texture, Vector2 startPosition)
     {
@@ -17,14 +21,33 @@
 
         if (this.isJumping)
         {
-            this.position.Y += this.jumpSpeed * deltaTime;
-            this.isJumping = false;  // Reset, um mehrere aufeinanderfolgende Sprünge zu verhindern
+            this.position.Y -= this.jumpSpeed * deltaTime;
+
+            // Aktualisiere den Sprung-Timer
+            this.jumpTimer += deltaTime;
+
+            if (this.jumpTimer >= this.jumpDuration)
+            {
+                this.isJumping = false;
+                this.jumpTimer = 0f;
+            }
+        }
+        else
+        {
+            // Der Charakter wird durch die Gravitation nach unten gezogen
+            this.position.Y += this.gravity * deltaTime;
+
+            // Kollisionsabfrage mit der festgelegten Y-Position (groundLevel)
+            if (this.position.Y + this.texture.Height > this.groundLevel)
+            {
+                this.position.Y = this.groundLevel - this.texture.Height;
+            }
         }
     }
 
     public void Jump()
     {
-        // Wenn der Charakter nicht bereits springt, springen ermöglichen
+        // Wenn der Charakter nicht bereits springt, starte den Sprung
         if (!this.isJumping)
         {
             this.isJumping = true;
